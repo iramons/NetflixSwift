b0e620053fe03c7b81633280251fa856837ea6ba
Marbet Ramon <tebram.dev@gmail.com>
2021-03-29 22:38:24 -0300

    first commit

---
+++ NetflixSwift/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  NetflixSwift
+//
+//  Created by Ramon Marbet on 29/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .red
+    }
+
+
+}
+
